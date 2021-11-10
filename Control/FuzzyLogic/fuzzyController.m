add4e6c342df5cc776250adeb6bcc28eefff6cf6
migue <migue@DESKTOP-7UIK5AI.mshome.net>
2021-11-09 20:13:08 -0600

    feat: adding second fis

--- Control/FuzzyLogic/fuzzyController.m
+++ Control/FuzzyLogic/fuzzyController.m
@@ -2,47 +2,93 @@
 % Decide temperature reference signal for a HVAC system based on 
 % time of the day and the difference between the building inside temperature and ambiental temperature
 
-fis = mamfis('Name','TemperatureSetter');
+fis1 = mamfis('Name','PrecalculateSetPointChange');
 % Inputs
-    fis = addInput(fis,[0 10],'Name','deltaT');
-            fis = addMF(fis,"deltaT","trapmf",[-1 0 1 2],"Name","chico");
-            fis = addMF(fis,"deltaT","trapmf",[1 2 3 4],"Name","mediano");
-            fis = addMF(fis,"deltaT","trapmf",[3 4 15 16],"Name","grande");
+    fis1 = addInput(fis1,[0    10],'Name','deltaT');
+            fis1 = addMF(fis1,"deltaT","trapmf",[-1 0 1 2],"Name","chico");
+            fis1 = addMF(fis1,"deltaT","trapmf",[1 2 3 4],"Name","mediano");
+            fis1 = addMF(fis1,"deltaT","trapmf",[3 4 15 16],"Name","grande");
             figure
-            plotmf(fis,"input",1)
+            plotmf(fis1,"input",1)
 
-    fis = addInput(fis,[0 24]*60,'Name','Horario');
-            fis = addMF(fis,"Horario","trapmf",[0 0 6 6.5]*60,"Name","B");
-            fis = addMF(fis,"Horario","trapmf",[6 6.5 19.5 20]*60,"Name","I1");
-            fis = addMF(fis,"Horario","trapmf",[19.5 20 22 22.5]*60,"Name","P");
-            fis = addMF(fis,"Horario","trapmf",[22 22.5 24 25]*60,"Name","I2");
+    fis1 = addInput(fis1,[0 24]*60,'Name','Horario');
+            fis1 = addMF(fis1,"Horario","trapmf",[0 0 6 6.5]*60,"Name","B");
+            fis1 = addMF(fis1,"Horario","trapmf",[6 6.5 19.5 20]*60,"Name","I1");
+            fis1 = addMF(fis1,"Horario","trapmf",[19.5 20 22 22.5]*60,"Name","P");
+            fis1 = addMF(fis1,"Horario","trapmf",[22 22.5 24 25]*60,"Name","I2");
             figure
-            plotmf(fis,"input",2)
+            plotmf(fis1,"input",2)
 
 % Output
-    fis = addOutput(fis,[-4 3],'Name','deltaSetPoint');
-          fis = addMF(fis,"deltaSetPoint","trapmf",[-5 -4 -3 -2],"Name","bajaleMucho");
-          fis = addMF(fis,"deltaSetPoint","trapmf",[-3 -2 -1 -0.5],"Name","bajale");
-          fis = addMF(fis,"deltaSetPoint","trapmf",[-1 -0.5 0.5 0.75],"Name","dejale");
-          fis = addMF(fis,"deltaSetPoint","trapmf",[0.5 0.75 3 4],"Name","subele");
+    fis1 = addOutput(fis1,[-4 3],'Name','deltaSetPoint');
+          fis1 = addMF(fis1,"deltaSetPoint","trapmf",[-5 -4 -3 -2],"Name","bajaleMucho");
+          fis1 = addMF(fis1,"deltaSetPoint","trapmf",[-3 -2 -1 -0.5],"Name","bajale");
+          fis1 = addMF(fis1,"deltaSetPoint","trapmf",[-1 -0.5 0.5 0.75],"Name","dejale");
+          fis1 = addMF(fis1,"deltaSetPoint","trapmf",[0.5 0.75 3 4],"Name","subele");
           figure
-          plotmf(fis,"output",1)
+          plotmf(fis1,"output",1)
 % Rules
-        rules = ["deltaT==chico & Horario==B => deltaSetPoint=bajale",... 
+        rules1 = ["deltaT==chico & Horario==B => deltaSetPoint=bajale",... 
             "deltaT==mediano & Horario==B => deltaSetPoint=bajale",... 
             "deltaT==grande & Horario==B => deltaSetPoint=bajaleMucho",...
             "deltaT==chico & Horario==I1 => deltaSetPoint=dejale",...
             "deltaT==chico & Horario==I2 => deltaSetPoint=dejale",...
             "deltaT==mediano & Horario==I1 => deltaSetPoint=dejale",...
             "deltaT==mediano & Horario==I2 => deltaSetPoint=dejale",...
-            "deltaT==grande & Horario==I1 => deltaSetPoint=dejale",...
-            "deltaT==grande & Horario==I2 => deltaSetPoint=dejale",...
+            "deltaT==grande & Horario==I1 => deltaSetPoint=bajale",...
+            "deltaT==grande & Horario==I2 => deltaSetPoint=bajale",...
             "deltaT==chico & Horario==P => deltaSetPoint=subele",...
             "deltaT==mediano & Horario==P => deltaSetPoint=subele",...
-            "deltaT==grande & Horario==P => deltaSetPoint=subele"];
-    fis = addRule(fis,rules);
+            "deltaT==grande & Horario==P => deltaSetPoint=dejale"];
+    fis1 = addRule(fis1,rules1);
     
     options = gensurfOptions;
     options.NumGridPoints = 50;
-    gensurf(fis,options)
+    gensurf(fis1,options)
+    
+fis2 = mamfis('Name','TemperatureSetter');
+% Inputs
+    fis2 = addInput(fis2,[-4 3],'Name','precalculated_deltaSetPoint');
+          fis2 = addMF(fis2,"precalculated_deltaSetPoint","trapmf",[-5 -4 -3 -2],"Name","bajaleMucho");
+          fis2 = addMF(fis2,"precalculated_deltaSetPoint","trapmf",[-3 -2 -1 -0.5],"Name","bajale");
+          fis2 = addMF(fis2,"precalculated_deltaSetPoint","trapmf",[-1 -0.5 0.5 0.75],"Name","dejale");
+          fis2 = addMF(fis2,"precalculated_deltaSetPoint","trapmf",[0.5 0.75 3 4],"Name","subele");
+          figure
+          plotmf(fis2,"input",1)
+
+    fis2 = addInput(fis2,[18 26],'Name','Comfort');
+            fis2 = addMF(fis2,"Comfort","zmf",[ 20 21],"Name","frio");
+            fis2 = addMF(fis2,"Comfort","trapmf",[21 22 24 25],"Name","bueno");
+            fis2 = addMF(fis2,"Comfort","smf",[24 25],"Name","caliente");
+            figure
+            plotmf(fis2,"input",2)
+
+% Output
+    fis2 = addOutput(fis2,[-4 3],'Name','deltaSetPoint');
+          fis2 = addMF(fis2,"deltaSetPoint","trapmf",[-5 -4 -3 -2],"Name","bajaleMucho");
+          fis2 = addMF(fis2,"deltaSetPoint","trapmf",[-3 -2 -1 -0.5],"Name","bajale");
+          fis2 = addMF(fis2,"deltaSetPoint","trapmf",[-1 -0.5 0.5 0.75],"Name","dejale");
+          fis2 = addMF(fis2,"deltaSetPoint","trapmf",[0.5 0.75 3 4],"Name","subele");
+          figure
+          plotmf(fis2,"output",1)
+    
+% Rules
+        rules2 = ["Comfort==frio     & precalculated_deltaSetPoint==bajaleMucho => deltaSetPoint=dejale",... 
+                  "Comfort==frio     & precalculated_deltaSetPoint==bajale      => deltaSetPoint=dejale",... 
+                  "Comfort==frio     & precalculated_deltaSetPoint==dejale      => deltaSetPoint=dejale",...
+                  "Comfort==frio     & precalculated_deltaSetPoint==subele      => deltaSetPoint=subele",...
+                  "Comfort==bueno    & precalculated_deltaSetPoint==bajaleMucho => deltaSetPoint=bajale",...
+                  "Comfort==bueno    & precalculated_deltaSetPoint==bajale      => deltaSetPoint=bajale",...
+                  "Comfort==bueno    & precalculated_deltaSetPoint==dejale      => deltaSetPoint=dejale",...
+                  "Comfort==bueno    & precalculated_deltaSetPoint==subele      => deltaSetPoint=subele",...
+                  "Comfort==caliente & precalculated_deltaSetPoint==bajaleMucho => deltaSetPoint=bajaleMucho",...
+                  "Comfort==caliente & precalculated_deltaSetPoint==bajale      => deltaSetPoint=bajale",...
+                  "Comfort==caliente & precalculated_deltaSetPoint==dejale      => deltaSetPoint=bajale",...
+                  "Comfort==caliente & precalculated_deltaSetPoint==subele      => deltaSetPoint=dejale"];
+    fis2 = addRule(fis2,rules2);
+    
+    options = gensurfOptions;
+    options.NumGridPoints = 50;
+    gensurf(fis2,options)
+    
     
